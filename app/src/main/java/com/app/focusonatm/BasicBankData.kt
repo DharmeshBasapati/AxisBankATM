6ccd789e3d86e8e08f3902610e2a439449cdc27f
Dharmesh Basapati <dharmesh.basapati@spacestem.com>
2021-10-06 19:58:53 +0530

    Updated App Icon & Some minor UI changes.

--- app/src/main/java/com/app/focusonatm/BasicBankData.kt
+++ app/src/main/java/com/app/focusonatm/BasicBankData.kt
@@ -3,7 +3,7 @@ package com.app.focusonatm
 object BasicBankData {
 
     const val BANK_ID = 1
-    const val BANK_BALANCE = 5000
+    const val BANK_BALANCE = 50000
     const val NOTES_OF_100 = 75
     const val NOTES_OF_200 = 50
     const val NOTES_OF_500 = 25
